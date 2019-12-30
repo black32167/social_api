de98c4151dad49c6ed1bd8b4c6c75477a30e9088
Black <psgrepjava@gmail.com>
2019-12-30 22:36:20 +1100

    testing API completness

--- server-mocks/src/test/java/social/api/test/server/ApiIntegrationTest.java
+++ server-mocks/src/test/java/social/api/test/server/ApiIntegrationTest.java
@@ -1,26 +1,69 @@
 package social.api.test.server;
 
+import org.junit.Before;
 import org.junit.Test;
 import social.api.stub.task.ApiClient;
 import social.api.stub.task.ApiException;
 import social.api.stub.task.client.TaskApi;
 import social.api.stub.task.model.Task;
+import social.api.stub.task.model.Tasks;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 public class ApiIntegrationTest {
     private static final String BASE_PATH = "http://127.0.0.1:8080";
 
-    @Test
-    public void testIntegration() throws ApiException, IOException {
-        ApiClient apiClient = new ApiClient().setBasePath(BASE_PATH);
-        TaskApi api = new TaskApi(apiClient);
-        Task task = api.taskGet();
+    private static final Task task1 = new Task()
+            .title("Find the cure")
+            .creator("John")
+            .assignee("Mary");
+    private static final Task task2 = new Task()
+            .title("Test the cure")
+            .creator("Mary")
+            .assignee("John");
 
-        assertNotNull(task);
-        assertEquals("user1", task.getCreator());
+    TaskApi api;
+
+    @Before
+    public void init() {
+        ApiClient apiClient = new ApiClient().setBasePath(BASE_PATH);
+        api = new TaskApi(apiClient);
+    }
+
+    @Test
+    public void testReturnTaskById() throws ApiException, IOException {
+        String postedTaskId = postTasks(task1, task2).get(1);
+
+        Task retrievedTask = api.taskTaskIdGet(postedTaskId);
+
+        assertEquals(postedTaskId, retrievedTask.getId());
+        assertEquals(task2.getCreator(), retrievedTask.getCreator());
+        assertEquals(task2.getAssignee(), retrievedTask.getAssignee());
+        assertEquals(task2.getTitle(), retrievedTask.getTitle());
+    }
+
+
+    @Test
+    public void testReturnAllTasks() throws ApiException, IOException {
+        postTasks(task1, task2);
+
+        Tasks retrievedTasksContainer = api.taskGet();
+        List<Task> retrievedTasks = retrievedTasksContainer.getTasks();
+
+        assertEquals(2, retrievedTasks.size());
+        assertEquals(Arrays.asList(task1, task2), retrievedTasks);
+    }
+
+    private List<String> postTasks(Task... tasks) throws ApiException {
+        List<String> taskIds = new ArrayList<String>();
+        for(Task task: tasks) {
+            taskIds.add(api.taskPost(task).getId());
+        }
+        return taskIds;
     }
 }

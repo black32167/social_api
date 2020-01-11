ddb25051729225bc3e900dd3833560703f27723d
Black <psgrepjava@gmail.com>
2020-01-11 15:42:51 +1100

    User validation endpoint implementation

--- server-mocks/src/main/kotlin/social/api/mock/user/UserApiServiceMock.kt
+++ server-mocks/src/main/kotlin/social/api/mock/user/UserApiServiceMock.kt
@@ -2,27 +2,52 @@ package social.api.mock.user
 
 import social.api.mock.ResettableMock
 import social.api.user.model.User
+import social.api.user.model.UserAuth
+import social.api.user.model.UserCredentials
 import social.api.user.model.Users
+import social.api.user.model.ValidationResult
 import social.api.user.server.UserApiService
+import java.nio.charset.StandardCharsets
 import java.util.*
+import javax.ws.rs.BadRequestException
 import javax.ws.rs.NotFoundException
 
 class UserApiServiceMock: UserApiService, ResettableMock {
+    val basicPrefix =  "Basic "
     val users = mutableMapOf<String, User>()
-    
-    override fun getUser(userId: String): User
-            = users[userId] ?:  throw NotFoundException()
+    val creds = mutableMapOf<String, UserCredentials>()
+
+    override fun setCredentials(userName: String, credentials: UserCredentials) {
+        creds[userName] = credentials;
+    }
+
+    override fun validate(auth: UserAuth): ValidationResult {
+        val authToken:String = auth.authToken ?: throw BadRequestException("Auth token was not supplied");
+        if(authToken.startsWith(basicPrefix)) {
+            val authToken = authToken.substring(basicPrefix.length)
+            val (userName, password) = String(Base64.getDecoder().decode(authToken.toByteArray(StandardCharsets.UTF_8))).split(':')
+            val knownUserCreds = creds[userName]
+            if(knownUserCreds?.password.equals(password)) {
+                return ValidationResult().valid(true)
+            }
+        }
+        return ValidationResult().valid(false)
+    }
+
+    override fun getUser(userName: String): User
+            = users[userName] ?:  throw NotFoundException()
 
     override fun getUsers(): Users
             = Users().users(users.values.toList())
 
     override fun createUser(user: User): User {
-        val userId = UUID.randomUUID().toString()
-        users[userId] = user.apply { id = userId }
+        val userName = user.name ?: throw BadRequestException("User name is not set")
+        users[userName] = user
         return user
     }
 
     override fun reset() {
         users.clear()
+        creds.clear()
     }
 }

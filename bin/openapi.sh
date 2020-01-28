abd1f71cfcb9980ecaebc16545edafa94564b0a6
Black <psgrepjava@gmail.com>
2020-01-28 23:28:21 +1100

    making maven download openapi generator

--- bin/openapi.sh
+++ bin/openapi.sh
@@ -10,7 +10,7 @@ cleanupDownload() {
 }
 
 installOPENAPICodegen() {
-    [ -f "${OPENAPI_CODEGEN_LOCAL}" ] || wget --no-check-certificate -O "${OPENAPI_CODEGEN_LOCAL}" "${OPENAPI_CODEGEN_CLI_URL}" || cleanupDownload
+    [ -f "${OPENAPI_CODEGEN_LOCAL}" ] || cp "${M2_REPO}/repository/org/openapitools/openapi-generator-cli/${OPENAPI_CODEGEN_VRSION}/openapi-generator-cli-${OPENAPI_CODEGEN_VRSION}.jar" "${OPENAPI_CODEGEN_LOCAL}"
 }
 
 ###############
@@ -20,13 +20,15 @@ installOPENAPICodegen() {
 SCRIPT_DIR=${BASH_SOURCE%/*}
 . ${SCRIPT_DIR}/shared.sh
 OPENAPI_CODEGEN_VRSION="4.2.2"
-OPENAPI_CODEGEN_CLI_URL="https://central.maven.org/maven2/org/openapitools/openapi-generator-cli/${OPENAPI_CODEGEN_VRSION}/openapi-generator-cli-${OPENAPI_CODEGEN_VRSION}.jar"
 OPENAPI_CODEGEN_LOCAL="${DOWNLOAD}/openapi-generator-cli-${OPENAPI_CODEGEN_VRSION}.jar"
 
 ###############
 ### Execution flow
 ###############
 
+: ${M2_REPO:=${HOME}/.m2}
+
+echo "M2_REPO=${M2_REPO}"
 installOPENAPICodegen
 
 java -DdebugOperations -DdebugModels -cp "${DOWNLOAD}/*" org.openapitools.codegen.OpenAPIGenerator "$@"

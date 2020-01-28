0b22459daeebf0efc85cb196ce2e571315639051
Black <psgrepjava@gmail.com>
2020-01-28 22:31:03 +1100

    fixed script (https)

--- bin/openapi.sh
+++ bin/openapi.sh
@@ -20,7 +20,7 @@ installOPENAPICodegen() {
 SCRIPT_DIR=${BASH_SOURCE%/*}
 . ${SCRIPT_DIR}/shared.sh
 OPENAPI_CODEGEN_VRSION="4.2.2"
-OPENAPI_CODEGEN_CLI_URL="http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/${OPENAPI_CODEGEN_VRSION}/openapi-generator-cli-${OPENAPI_CODEGEN_VRSION}.jar"
+OPENAPI_CODEGEN_CLI_URL="https://central.maven.org/maven2/org/openapitools/openapi-generator-cli/${OPENAPI_CODEGEN_VRSION}/openapi-generator-cli-${OPENAPI_CODEGEN_VRSION}.jar"
 OPENAPI_CODEGEN_LOCAL="${DOWNLOAD}/openapi-generator-cli-${OPENAPI_CODEGEN_VRSION}.jar"
 
 ###############
